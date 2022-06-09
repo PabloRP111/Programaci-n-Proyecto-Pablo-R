442eea58abd2eb15a92f2e442ab5fb53bc21c404
PabloRP <92531467+PabloRP111@users.noreply.github.com>
2022-06-09 14:45:07 +0200

    v2
    v2

---
+++ Pokemon Alhandalus/Pokemon_Alhandalus/src/main/java/interfacesGraficas/PantallaInicio.java
@@ -0,0 +1,15 @@
+package interfacesGraficas;
+
+import javax.swing.JPanel;
+
+public class PantallaInicio extends JPanel{
+
+	private Ventana ventana;
+	
+	public PantallaInicio(Ventana v){
+		
+		ventana=v;
+		
+	}
+	
+}
--- Pokemon Alhandalus/Pokemon_Alhandalus/src/main/java/principal/Main.java
+++ Pokemon Alhandalus/Pokemon_Alhandalus/src/main/java/principal/Main.java
@@ -1,9 +1,12 @@
 package principal;
 
+import interfacesGraficas.Ventana;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		Ventana ventana=new Ventana();
 
 	}
 
